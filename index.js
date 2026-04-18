fc9309954f35b89e1d8effe981d6ac2557230292
Tony Wong <tdubb123@gmail.com>
2026-04-18 10:28:58 -0700

    feat: add discount calculator

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function splitBill(bill, people) {
 }
 
 module.exports = { tipCalculator, splitBill };
+
+function applyDiscount(price, discountPercent) {
+  return price - discountPercent / 100;  // bug: should be price * discountPercent / 100
+}
+
+module.exports.applyDiscount = applyDiscount;
